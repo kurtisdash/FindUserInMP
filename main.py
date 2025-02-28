b3643d8b46d5e5dad5df997895b03f35ca0a70a5
Kurtis <91085234+kurtisdash@users.noreply.github.com>
2025-02-28 04:40:42 -0500

    made loop 1000, made option to check next 1000, made faster

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from config import API_KEY
 from api import fetch_match_data
 from utils import get_player_input, check_player_in_match
 
+OSU_DEFAULT_MP_URL = 'https://osu.ppy.sh/community/matches/'
+api_url = "https://osu.ppy.sh/api/get_match"
+mp, player_id = get_player_input()
+
 def make_api_request():
-    OSU_DEFAULT_MP_URL = 'https://osu.ppy.sh/community/matches/'
-    api_url = "https://osu.ppy.sh/api/get_match"
+    global mp
+    global player_id
 
-    mp, player_id = get_player_input()
-
-    print(f"\nChecking the last 100 MPs from {OSU_DEFAULT_MP_URL}{mp} to {OSU_DEFAULT_MP_URL}{mp-100}."
-          f"\nPlease be patient. Checking all 100 previous matches takes about 3 and a half minutes."
+    print(f"\nChecking the last 1000 MPs from {OSU_DEFAULT_MP_URL}{mp} to {OSU_DEFAULT_MP_URL}{mp-1000}."
+          f"\nPlease be patient. Checking all 1000 previous matches takes around 22 minutes."
           f"\nIf the player is found before that, the process will stop early.")
 
-    for _ in range(100):
+    for _ in range(1000):
         players = set()  # Clear the set after each match
         match_data = fetch_match_data(api_url, mp, API_KEY)
 
@@ -27,11 +29,11 @@ def make_api_request():
                 return
 
         mp -= 1  
-        time.sleep(2)  # Prevent rate limiting
+        time.sleep(1.3)  # Prevent rate limiting
 
-    print('\nPlayer not found in the last 100 matches.')
+    print(f'\nPlayer not found in the last 1000 matches. Last MP link checked: {OSU_DEFAULT_MP_URL}{mp}')
 
-    repeat = input('Would you like to try the next 100 matches? (y/n): ')
+    repeat = input('Would you like to try the next 1000? (y/n): ')
     if repeat.lower() == 'y':
         make_api_request()  
 
